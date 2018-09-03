80c9e810275ef90996aed02cc7f6bfd2beccd209
greyshell <grey.shell@gmail.com>
2018-09-02 22:24:24 -0700

    improving the nos of function invocation

--- leetcode/tower_of_hanoi.py
+++ leetcode/tower_of_hanoi.py
@@ -18,14 +18,21 @@ class Solution(object):
 
     def toh(self, n, x, y, z):
         """
-        description: time complexity: O(2^n), space complexity:
-        :param n:
-        :param x:
-        :param y:
-        :param z:
-        :return:
+        tower of hanoi
+        time complexity: O(2^(n+1) -1 ) ~ O(2^n)
+        space complexity: O(n-1)
+        nos of recursive function invocation: 2^n - 1,  considering the base case => I(n) = 1 where n = 1
+        nos of movement being done: 2^n -1, considering the base case => M(n) = 0 where n = 0
+
+        :param n: int
+        :param x: char
+        :param y: char
+        :param z: char
+        :return: void / none
         """
-        if n >= 1:
+        if n == 1:  # improve the nos of function invocation
+            print "[+] move disk from %s to %s" % (x, y)
+        elif n > 1:
             self.toh(n-1, x, z, y)
             print "[+] move disk from %s to %s" % (x, y)
             self.toh(n - 1, z, y, x)
@@ -35,8 +42,8 @@ class Solution(object):
 
 def main():
     s = Solution()
-    # s.toh(3, 'x', 'y', 'z')
-    s.toh(5, 'x', 'y', 'z')
+    s.toh(3, 'x', 'y', 'z')
+    # s.toh(5, 'x', 'y', 'z')
 
 
 if __name__ == '__main__':
