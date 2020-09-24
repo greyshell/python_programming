d80c8c092cf8829d991728b89843efd8bed3fa8d
Abhijit Sinha <grey.shell@gmail.com>
2020-09-24 12:21:49 -0400

    learning dict

--- ds_algo/dict/test/test_dict.py
+++ ds_algo/dict/test/test_dict.py
@@ -28,7 +28,7 @@ def main():
         print(f"error: {e}")
 
     # look up: method 2
-    if "no_key" in name_dept:  # inefficient because it queries the dict twice
+    if "key" in name_dept:  # inefficient because it queries the dict twice
         dept = name_dept["no_key"]
         print(f"{dept}")
 
@@ -36,7 +36,20 @@ def main():
     dept = name_dept.get("alice", "no_key")  # provides the support for default value
     print(f"{dept}")
 
-    # add items
+    # get all keys
+    print(f"{name_dept.keys()}")  # retuens a object
+    # get all values
+    print(f"{name_dept.values()}")  # returns a object
+    # get key and values in tuple format
+    print(f"{name_dept.items()}")  # returns a list, each entry is tuple
+
+    # print the dict: method 1
+    for key, value in name_dept.items():
+        print(f"{key}: {value}")
+
+    # print the dict: method 2
+    for key, value in name_dept:
+        print(f"{key}: {name_dept[key]}")
 
 
 if __name__ == '__main__':
