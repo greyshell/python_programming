29b8fe943753e9c44ed85fe8a17f580592c59da4
ABHIJIT SINHA <grey.shell@gmail.com>
2021-08-25 21:10:41 -0400

    added test_pwn_utils_lib.py
    Signed-off-by: ABHIJIT SINHA <grey.shell@gmail.com>

---
+++ core_concepts/test_pwn_utils_lib.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+
+# author: greyshell
+# how to use: python test_pwn_utils_lib.py -m true local -b vuln -g true
+
+from pwn import *
+from pwn_utils import PwnUtils
+
+
+def exploit(conn):
+    """
+    interact with the binary with some valid input
+    """
+    print("success")
+
+
+def main():
+    my_input = PwnUtils()
+    arguments = my_input.parser.parse_args()
+    connection = ""
+
+    # run the script without any argument
+    if len(sys.argv) == 1:
+        my_input.parser.print_help(sys.stderr)
+        sys.exit(1)
+
+    # exploiting local binary
+    if arguments.command == 'local':
+        binary_name = "./"
+        binary_name += arguments.binary
+        connection = process([binary_name])
+        # attach the binary with gdb in tmux session
+        if arguments.gdb == 'true':
+            gdb.attach(connection)
+
+    elif arguments.command == 'network':
+        connection = remote(arguments.ip_address, arguments.port)
+
+    if arguments.debug_mode == 'true':
+        context.log_level = 'debug'
+
+    # invoke the exploit function
+    exploit(connection)
+
+
+if __name__ == '__main__':
+    main()
